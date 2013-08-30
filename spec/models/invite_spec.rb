5a5793f91064fd88acacf37fb608175d39a6585c
Alvis Duong <alvisduong@gmail.com>
2013-08-30 17:58:40 +0100

    Ensure key length is greater than 16

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
 
 describe Invite do
-  #pending "add some examples to (or delete) #{__FILE__}"
-  it 'should always have a key of resonable length after creation' do
-    new_invite = Invite.new
-    should ensure_length_of(new_invite.key).is_at_least(16)
-      .with_message('key is too short')
+  it 'should always have a key of reasonable length after creation' do
+    invite = Invite.new
+    invite.key.should_not be_empty
+    invite.key.should satisfy { |key| key.length > 16 }
   end
 end
