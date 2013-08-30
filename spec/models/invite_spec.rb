a08bd6865676515b8f067d5428927562e2da040d
Alvis Duong <alvisduong@gmail.com>
2013-08-30 20:08:41 +0100

    Test that description must be provided

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -6,4 +6,8 @@ describe Invite do
     invite.key.should_not be_empty
     invite.key.should satisfy { |key| key.length > 16 }
   end
+
+  it 'should require a description' do
+    invite = Invite.create(:description => '').should_not be_valid
+  end
 end
