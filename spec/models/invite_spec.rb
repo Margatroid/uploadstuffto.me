94a88013d50cf31e610262b065bbaa5882233f72
Alvis Duong <alvisduong@gmail.com>
2013-08-30 17:12:16 +0100

    Test invite always having 16 character key upon instantiation

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Invite do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it 'should always have a key after creation' do
+    new_invite = Invite.new
+    expect(new_invite.key).to have(16).characters
+  end
 end
