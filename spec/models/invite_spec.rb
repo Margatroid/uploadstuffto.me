e044bc0f35b7972c23a4b6a5647576b1b79031e9
Alvis Duong <alvisduong@gmail.com>
2013-08-30 20:43:40 +0100

    Refactor and check key length validation

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
+min_key_length = 16
+
 describe Invite do
+  before  { @invite = Invite.new(:description => 'Test invite') }
+  subject { @invite }
+
   it 'should always have a key of reasonable length after creation' do
-    invite = Invite.new
-    invite.key.should_not be_empty
-    invite.key.should satisfy { |key| key.length > 16 }
+    subject.key.should_not be_empty
+    subject.key.should satisfy { |key| key.length > min_key_length }
   end
 
   it 'should require a description' do
     invite = Invite.create(:description => '').should_not be_valid
+  end
+
+  it "should consider keys shorter than #{ min_key_length } to be invalid" do
+    subject.key = 'hello'
+    expect { subject.save! }.to raise_error(RecordInvalid)
   end
 end
