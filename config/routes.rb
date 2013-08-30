d1614a2213b988aabb8b6c6ca68183020f1e448a
Alvis Duong <alvisduong@gmail.com>
2013-08-30 16:38:00 +0100

    Generate home controller and update routes to use it

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ NoMoreIdeas::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#root'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
