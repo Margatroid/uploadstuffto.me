b110ff57e74195c17089c470c817b4ffa7d2cc92
Alvis Duong <alvisduong@gmail.com>
2013-08-30 17:35:33 +0100

    Ensure key is at least 16 characters long

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe Invite do
   #pending "add some examples to (or delete) #{__FILE__}"
-  it 'should always have a key after creation' do
+  it 'should always have a key of resonable length after creation' do
     new_invite = Invite.new
-    expect(new_invite.key).to have(16).characters
+    should ensure_length_of(new_invite.key).is_at_least(16)
+      .with_message('key is too short')
   end
 end
