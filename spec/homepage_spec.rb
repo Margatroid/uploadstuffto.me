6ddcf1ad17413d8f529a3844d9f9f71c2c2ac6f8
Alvis Duong <alvisduong@gmail.com>
2013-08-30 16:41:53 +0100

    Rename test description

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'homepage', :type => :feature do
+describe 'homepage behaviour', :type => :feature do
   it "loads a page with the site's name" do
     visit '/'
     expect(page).to have_content 'No more ideas'
