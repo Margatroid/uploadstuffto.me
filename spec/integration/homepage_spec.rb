e8d8b00913f88f887d8efdb28158f2dbd07456eb
Alvis Duong <alvisduong@gmail.com>
2013-09-08 16:05:02 +0100

    Removing pending tests, add new empty tests

--- spec/integration/homepage_spec.rb
+++ spec/integration/homepage_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe 'homepage behaviour', :type => :feature do
+  before { visit '/' }
   it "loads a page with the site's name" do
-    visit '/'
     expect(page).to have_content 'No more ideas'
+  end
+
+  it 'will display an upload field if logged in' do
+  end
+
+  it 'will not display an upload field if logged out' do
   end
 end
