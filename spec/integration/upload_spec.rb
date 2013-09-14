fc1bf623f1cc63c6e8e4f7f3d3a62862a739cd86
Alvis Duong <alvisduong@gmail.com>
2013-09-14 16:09:54 +0100

    Use eq instead of should_be, fix expected value

--- spec/integration/upload_spec.rb
+++ spec/integration/upload_spec.rb
@@ -14,7 +14,7 @@ describe 'homepage upload', :type => :feature do
     attach_file('File', File.expand_path('spec/assets/chicken_rice.jpg'))
 
     click_button 'Upload image'
-    current_path.should_be image_path
+    current_path.should eq(image_path(1))
     expect(page).to have_content 'Image uploaded successfully'
   end
 
