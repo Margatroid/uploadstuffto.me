8108264f74dbab312a64ac232dcc4eae310624cb
Alvis Duong <alvisduong@gmail.com>
2013-09-14 22:36:09 +0100

    Fix typo, add file and user ID

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe Image do
+  let(:valid_attributes) do
+    extend ActionDispatch::TestProcess
+    { :user_id => 1, :file => fixture_file_upload('chicken_rice.jpg') }
+  end
+
   it 'will have a key' do
-    invite = Invite.create()
-    invite.key should exist
+    image = Image.create! valid_attributes
+    image.key should exist
   end
 end
