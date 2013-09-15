30851a585981a69461c84f84695bbe09061ec91c
Alvis Duong <alvisduong@gmail.com>
2013-09-15 11:08:07 +0100

    Name some routes to get the helpers working again

--- config/routes.rb
+++ config/routes.rb
@@ -21,10 +21,10 @@ NoMoreIdeas::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  get '/images'           => 'images#index'
+  get '/images'           => 'images#index',   as: 'images'
   post '/images'          => 'images#create'
-  get '/images/:key'      => 'images#show'
-  get '/images/:key/edit' => 'images#edit'
+  get '/images/:key'      => 'images#show',    as: 'image'
+  get '/images/:key/edit' => 'images#edit',    as: 'edit_image'
   put '/images/:key'      => 'images#update'
   delete '/images/:key'   => 'images#destroy'
 
