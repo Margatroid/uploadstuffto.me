c2c428d597277bf684f5bf8bdee277160c1136c7
Alvis Duong <alvisduong@gmail.com>
2013-09-15 09:30:04 +0100

    Refactor to move upload logic away from test case

--- spec/integration/upload_spec.rb
+++ spec/integration/upload_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
+module UploadHelper
+  def upload_file
+    visit '/'
+    attach_file('File', File.expand_path('spec/fixtures/chicken_rice.jpg'))
+  end
+end
+
 describe 'homepage upload', :type => :feature do
+  include UploadHelper
+
   before(:each) do
     @registered_user = Invite.create(:description => 'even')
       .users
@@ -10,12 +19,14 @@ describe 'homepage upload', :type => :feature do
   end
 
   it 'will let me upload an image from disk' do
-    visit '/'
-    attach_file('File', File.expand_path('spec/fixtures/chicken_rice.jpg'))
+    upload_file
 
     click_button 'Upload image'
     current_path.should eq(image_path(1))
     expect(page).to have_content 'Image uploaded successfully'
+  end
+
+  it 'will have a working short link after uploading an image' do
   end
 
   after(:each) do
