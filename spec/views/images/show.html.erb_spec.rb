65f49a5769831afaa3f65a6596f5e97bef001f31
Alvis Duong <alvisduong@gmail.com>
2013-09-21 11:04:39 +0100

    Test image has correct source

--- spec/views/images/show.html.erb_spec.rb
+++ spec/views/images/show.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "images/show" do
+describe "images/show", :type => :feature do
   before(:each) do
     @image = assign(:image, stub_model(Image))
   end
@@ -8,5 +8,11 @@ describe "images/show" do
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+  end
+
+  it 'has a correct image' do
+    img = page.first(:css, '#fullsize_image')
+    visit img[:src]
+    page.status_code.should be 200
   end
 end
