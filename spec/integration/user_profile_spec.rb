3530d8a14f8ae1d0ace70cfc655e1b210fbb3ba9
Alvis Duong <alvisduong@gmail.com>
2013-09-22 20:02:22 +0100

    Start writing a spec for the user profile

---
+++ spec/integration/user_profile_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+include UploadHelper, UserFactory
+
+describe 'user profile header', :type => :feature do
+  it "shows the user's name" do
+    pending "Don't even have a username field in DB yet."
+  end
+end
