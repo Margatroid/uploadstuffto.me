8edd7c97c5276e1f7641f8ad2664c8b52c70688b
Alvis Duong <alvisduong@gmail.com>
2013-09-22 21:36:02 +0100

    Test username validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,27 @@ require 'spec_helper'
 describe User do
   #pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe 'username validation' do
+  it 'will not accept spaces' do
+    user = create(:invite).users.build(
+      username: 'This username has spaces',
+      email: 'thisisanemail@rspec.com',
+      password: 'helloworld'
+    )
+
+    user.save.should eq(false)
+    User.count.should eq(0)
+  end
+
+  it 'will accept alphanumerics, underscores and hyphens' do
+      user = create(:invite).users.build(
+      username: 'azAZ0123456789_-alice_m',
+      email: 'thisisanemail@rspec.com',
+      password: 'helloworld'
+    )
+
+    user.save.should eq(true)
+    User.count.should eq(1)
+  end
+end
