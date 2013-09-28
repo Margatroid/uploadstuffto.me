b936bf8a14663c07b2e484b094b8aaa0724832fe
Alvis Duong <alvisduong@gmail.com>
2013-09-28 16:13:19 +0100

    Test image counts on user profiles

--- spec/integration/user_profile_spec.rb
+++ spec/integration/user_profile_spec.rb
@@ -6,22 +6,42 @@ describe 'correct routing to profile', :type => :feature do
   it 'will load the profile at the correct path' do
     user = create(:user)
     visit "/profile/#{ user.username }"
-    expect(page).to have_content user.username
+    page.status_code.should be 200
   end
 end
 
 describe 'user profile header', :type => :feature do
+  before(:each) do
+    @user = create(:user)
+    login_as(@user, :scope => :user)
+
+    4.times { |i| upload_test_file }
+    2.times { |i| upload_test_another_file }
+
+    logout(:user)
+
+    visit "/profile/#{ @user.username }"
+  end
+
   it "shows the user's name" do
+    expect(page).to have_content @user.username
   end
 
   context 'when logged in' do
     it 'shows how many images you have' do
+      login_as(@user, :scope => :user)
+      expect(page).to have_content '6 images'
     end
   end
 
   context 'when logged out' do
     it 'shows how many public images you have' do
     end
+  end
+
+  after(:each) do
+    logout(:user)
+    Warden.test_reset!
   end
 end
 
