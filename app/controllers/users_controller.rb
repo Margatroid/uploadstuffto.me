66d38865c0bc7de02d74eae3080f9ab8e9529100
Alvis Duong <alvisduong@gmail.com>
2013-09-28 17:20:16 +0100

    Show recent uploads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by_username(params[:username])
+    @user           = User.find_by_username(params[:username])
+    @recent_uploads = @user.recently_uploaded(50)
   end
 end
