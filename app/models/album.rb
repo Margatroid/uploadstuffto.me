eb2c0d50705466a4dff423752143112a86d6752f
Alvis Duong <alvisduong@gmail.com>
2013-10-19 14:15:16 +0100

    Add associations

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,5 @@ class Album < ActiveRecord::Base
   validates :user_id, presence: true
 
   belongs_to :user
+  has_many :album_images
 end
--- app/models/album_image.rb
+++ app/models/album_image.rb
@@ -1,2 +1,7 @@
 class AlbumImage < ActiveRecord::Base
+  belongs_to :user
+  has_one :image
+
+  validates :album_id, presence: true
+  validates :image_id, presence: true
 end
