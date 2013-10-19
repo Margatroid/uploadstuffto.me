3d7bcccb6db9019c9bc234665bc5df3352690a6c
Alvis Duong <alvisduong@gmail.com>
2013-10-19 11:33:46 +0100

    Spec out some pending Album and AlbumImage tests

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Album do
+  it 'must belong to a user' do
+    pending 'test user association'
+  end
+
+  it 'must be deleted if the user is deleted' do
+    pending 'test that album is gone if user is deleted'
+  end
 end
