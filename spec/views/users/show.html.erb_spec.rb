909b56fc766522e0df8f1b328b9bdaac33eda18f
Alvis Duong <alvisduong@gmail.com>
2013-10-19 21:43:38 +0100

    Spec out some view tests for the new edit mode

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -7,4 +7,42 @@ describe "users/show.html.erb" do
     render template: 'users/show'
     rendered.should have_text("berg_katze's profile")
   end
+
+  context 'when logged in' do
+    before(:each) do
+      @me  = create(:user_with_image)
+      @you = create(:user_with_image)
+      login_as(@me, :scope => :user)
+    end
+
+    it 'will show edit mode on your profile' do
+      assign(:user, @me)
+      assign(:recent_uploads, @me.recently_uploaded_paginate(1))
+      render template: 'users/show'
+      rendered.should have_text('Edit mode')
+    end
+
+    it "will not show edit mode on someone else's profile" do
+      assign(:user, @you)
+      assign(:recent_uploads, @you.recently_uploaded_paginate(1))
+      render template: 'users/show'
+      rendered.should_not have_text('Edit mode')
+    end
+
+    after(:each) do
+      logout(:user)
+      Warden.test_reset!
+    end
+  end
+
+  context 'when logged out' do
+    it 'will not show any editing controls' do
+      @me = create(:user_with_image)
+      assign(:user, @me)
+      assign(:recent_uploads, @me.recently_uploaded_paginate(1))
+
+      render template: 'users/show'
+      rendered.should_not have_text('Edit mode')
+    end
+  end
 end
