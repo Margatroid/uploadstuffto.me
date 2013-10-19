f97d1eaa294df5ad2e843b0c312ff1bb50b73746
Alvis Duong <alvisduong@gmail.com>
2013-10-19 11:55:25 +0100

    Test the validation of user_id on Album model

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe Album do
-  it 'must belong to a user' do
-    pending 'test user association'
-  end
+  it { should validate_presence_of :user_id }
 
   it 'must be deleted if the user is deleted' do
     pending 'test that album is gone if user is deleted'
