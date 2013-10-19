7bca347265b63f8765e61d1b3973521fe0f43cec
Alvis Duong <alvisduong@gmail.com>
2013-10-19 14:11:23 +0100

    Remove tests for standard associations

--- spec/models/album_image_spec.rb
+++ spec/models/album_image_spec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe AlbumImage do
-  it 'must belong to an album' do
-    pending 'test album image and album association'
-  end
-
-  it 'must have an image' do
-    pending 'test that album image contains an image'
-  end
-
   it 'must be destroyed if an image is deleted' do
     pending 'ensure this is destroyed if an image is deleted'
   end
