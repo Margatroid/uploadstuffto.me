e172d123e833ce083c1419311defef324670d4f4
Alvis Duong <alvisduong@gmail.com>
2013-10-20 15:57:54 +0100

    Add checkboxes for each tile in edit mode

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,5 +1,6 @@
 module ImagesHelper
   def show_edit_mode?
+    return unless @user
     user_signed_in? && @user.images.count > 0 && current_user == @user
   end
 
@@ -11,5 +12,11 @@ module ImagesHelper
     if collection.total_pages > 1
       render 'images/gallery_pagination', :collection => collection
     end
+  end
+
+  def edit_mode_checkbox(image)
+    return unless show_edit_mode? && edit_mode?
+    sprintf('<input type="checkbox" name="selected[]" value="%d" />',
+            image.id).html_safe
   end
 end
