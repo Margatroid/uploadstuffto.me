358f92fe8ed774daf0be579d0ef02a8c30bf735f
Alvis Duong <alvisduong@gmail.com>
2013-10-26 17:20:46 +0100

    Login/logout before each test

--- spec/acceptance/edit_mode_spec.rb
+++ spec/acceptance/edit_mode_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe 'deletion', :type => :feature do
+  before(:each) do
+    @me = create(:user_with_images)
+    login_as(@me, :scope => :user)
+  end
+
   it 'lets you delete an image' do
   end
 
   it 'lets you delete multiple images' do
+  end
+
+  after(:each) do
+    logout(:user)
+    Warden.test_reset!
   end
 end
