384e58a6102b92bb741780720d2ee10d23b6a120
Alvis Duong <alvisduong@gmail.com>
2013-10-26 21:24:29 +0100

    Acceptance test for deleting one image

--- spec/acceptance/edit_mode_spec.rb
+++ spec/acceptance/edit_mode_spec.rb
@@ -7,6 +7,17 @@ describe 'deletion', :type => :feature do
   end
 
   it 'lets you delete an image' do
+    visit "/users/#{ @me.username }"
+    click_link 'Edit mode'
+    # Find last image, @me.images.first image is on page 2.
+    page.find(:xpath, "//a[@href='#{ image_path(@me.images.last) }'][1]").click
+
+    images = Image.count
+
+    click_link 'Delete'
+    page.should have_content('Image(s) deleted.')
+
+    Image.count.should eq(images - 1)
   end
 
   it 'lets you delete multiple images' do
