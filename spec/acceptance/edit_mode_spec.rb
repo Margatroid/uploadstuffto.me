4971d72c6c2ba583e451ed5d0e25a9970f648a31
Alvis Duong <alvisduong@gmail.com>
2013-10-26 22:37:31 +0100

    Test deletion of many images

--- spec/acceptance/edit_mode_spec.rb
+++ spec/acceptance/edit_mode_spec.rb
@@ -21,6 +21,19 @@ describe 'deletion', :type => :feature do
     Image.count.should eq(images - 1)
   end
 
+  it 'lets you delete many images' do
+    @me.images.last(3).each do |image|
+      find("#tile_#{ image.key } .tile-select").set(true)
+    end
+
+    images = Image.count
+
+    click_button 'Delete'
+    page.should have_content('Image(s) deleted.')
+
+    Image.count.should eq(images - 3)
+  end
+
   after(:each) do
     logout(:user)
     Warden.test_reset!
