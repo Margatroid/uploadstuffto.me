695c62d6e1168a5f19ba8a3de2e50caba47fd9c4
Alvis Duong <alvisduong@gmail.com>
2013-10-27 15:23:17 +0000

    Fix association

--- app/models/album_image.rb
+++ app/models/album_image.rb
@@ -1,7 +1,7 @@
 class AlbumImage < ActiveRecord::Base
   belongs_to :album
+  belongs_to :image
   acts_as_list scope: :album
-  has_one :image
 
   validates :album_id, presence: true
   validates :image_id, presence: true
