acf4f6751567d3d60fe844346a60372582e05a02
Alvis Duong <alvisduong@gmail.com>
2013-11-01 22:07:07 +0000

    Test only showing recent uploads from featured profiles

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -13,3 +13,28 @@ describe Image do
     Image.first.key.should match(/\S{6}/)
   end
 end
+
+describe 'never show recent uploads from non-featured profiles' do
+  let(:featured_valid_attributes) do
+    extend ActionDispatch::TestProcess
+    { :user_id => 1, :file => fixture_file_upload('chicken_rice.jpg') }
+  end
+
+  let(:valid_attributes) do
+    extend ActionDispatch::TestProcess
+    { :user_id => 2, :file => fixture_file_upload('chicken_rice.jpg') }
+  end
+
+  it 'will never get recent images from non-featured profiles' do
+    show_on_homepage = Image.create! featured_valid_attributes
+    cannot_show_on_homepage = Image.create! valid_attributes
+
+    featured_profile = create(:user) # User ID 1.
+    regular_profile  = create(:user) # User ID 2.
+
+    featured_profile.featured = true
+    featured_profile.save
+
+    Image.recently_uploaded.should eq([show_on_homepage])
+  end
+end
