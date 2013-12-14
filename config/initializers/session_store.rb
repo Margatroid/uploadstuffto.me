50d75304db7d2df314d88177269a309849cf7b64
Alvis Duong <alvisduong@gmail.com>
2013-12-14 15:28:35 +0000

    Update configuration for new Rails update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-NoMoreIdeas::Application.config.session_store :cookie_store, key: '_no-more-ideas_session'
+NoMoreIdeas::Application.config.session_store :cookie_store, key: '_no_more_ideas_session'
