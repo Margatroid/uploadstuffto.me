85b5777a0c55dec10824e1334c28d09bc931812e
Alvis Duong <alvisduong@gmail.com>
2013-12-15 17:29:28 +0000

    Show image that's being edited, along with a dirty test to ensure it rendered

--- spec/views/images/edit.html.erb_spec.rb
+++ spec/views/images/edit.html.erb_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe "images/edit" do
   before(:each) do
-    @image = assign(:image, stub_model(Image))
-    @image.key = 'hello'
+    @image = assign(:image, stub_model(Image, :user_id => 1))
+    @image.file = File.open('public/test_image.jpg') # Dirty
+    @image.save!
   end
 
   it "renders the edit image form" do
@@ -17,6 +18,6 @@ describe "images/edit" do
 
   it "has a thumbnail so you don't forget what you're editing" do
     render
-    assert_select "img[src='/thumb/#{ @image.key }.jpg']"
+    assert_select "img[src='/thumb/test_imagejpg']"
   end
 end
