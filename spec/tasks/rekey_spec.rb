438b2ea7f77c1aab0fd27406ba6a72b8c1088437
Alvis Duong <alvisduong@gmail.com>
2013-12-20 14:33:15 +0000

    Start defining requirements of task

---
+++ spec/tasks/rekey_spec.rb
@@ -0,0 +1,12 @@
+describe 'rekey:all' do
+  include_context 'rake'
+
+  it 'rekeys images' do
+  end
+
+  it 'will rename thumbnail files' do
+  end
+
+  it 'will rename original image files' do
+  end
+end
