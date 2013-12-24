f6a427d0e1fce46347658c54179287a25695e76b
Alvis Duong <alvisduong@gmail.com>
2013-12-24 15:36:42 +0000

    Refactoring: clean up tests

--- spec/views/images/show.html.erb_spec.rb
+++ spec/views/images/show.html.erb_spec.rb
@@ -12,29 +12,25 @@ describe "images/show" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
   end
 
-  it 'shows the user that uploaded this image' do
-    uploader = create(:user)
+  it 'shows the user that uploaded this public image' do
     path_to_uploader =
-      Rails.application.routes.url_helpers.user_path(uploader)
+      Rails.application.routes.url_helpers.user_path(@image.user)
 
     @image.public  = true
-    @image.user = uploader
 
     render
-    rendered.should have_text(uploader.username)
+    rendered.should have_text(@image.user.username)
     rendered.should have_selector("a[href='#{ path_to_uploader }']")
   end
 
   it 'will not link to uploader if image is private' do
-    uploader = create(:user)
     path_to_uploader =
-      Rails.application.routes.url_helpers.user_path(uploader)
+      Rails.application.routes.url_helpers.user_path(@image.user)
 
     @image.public  = false
-    @image.user = uploader
 
     render
-    rendered.should_not have_text(uploader.username)
+    rendered.should_not have_text(@image.user.username)
     rendered.should_not have_selector("a[href='#{ path_to_uploader }']")
   end
 end
