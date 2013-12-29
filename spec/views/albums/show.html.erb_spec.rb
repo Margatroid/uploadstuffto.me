271e90702270df49734e1af17237570173bf727e
Alvis Duong <alvisduong@gmail.com>
2013-12-29 14:56:14 +0000

    Expect how the edit link should behave

--- spec/views/albums/show.html.erb_spec.rb
+++ spec/views/albums/show.html.erb_spec.rb
@@ -2,9 +2,32 @@ require 'spec_helper'
 
 describe "albums/show" do
   before(:each) do
-    @album = assign(:album, stub_model(Album,
-      :title => "Title"
-    ))
+    @album = assign(:album, stub_model(Album, title: "Title", key: 'abcde', user_id: 1))
+  end
+
+  context 'when logged in and viewing own album' do
+    it 'links to album edit' do
+      render
+      expect(rendered).to have_link('Edit album', href: edit_album_path(@album.key))
+    end
+  end
+
+  context 'when logged out' do
+    it 'does not link to album edit' do
+      view.stub(:current_user).and_return(nil)
+
+      render
+      expect(rendered).to_not have_link('Edit album', href: edit_album_path(@album.key))
+    end
+  end
+
+  context 'when logged in but not viewing own album' do
+    it 'does not link to album edit' do
+      view.stub(:current_user).and_return({ user_id: 2 })
+
+      render
+      expect(rendered).to_not have_link('Edit album', href: edit_album_path(@album.key))
+    end
   end
 
   it "renders attributes in <p>" do
