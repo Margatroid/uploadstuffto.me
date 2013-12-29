b857b30642c8b07075265d6b6ff18fc07697e2af
Alvis Duong <alvisduong@gmail.com>
2013-12-29 15:02:13 +0000

    Show edit link when viewing own album

--- spec/views/albums/show.html.erb_spec.rb
+++ spec/views/albums/show.html.erb_spec.rb
@@ -7,15 +7,15 @@ describe "albums/show" do
 
   context 'when logged in and viewing own album' do
     it 'links to album edit' do
+      view.stub(:current_user).and_return(create(:user))
       render
-      expect(rendered).to have_link('Edit album', href: edit_album_path(@album.key))
+      expect(rendered).to have_link('Edit', href: edit_album_path(@album.key))
     end
   end
 
   context 'when logged out' do
     it 'does not link to album edit' do
       view.stub(:current_user).and_return(nil)
-
       render
       expect(rendered).to_not have_link('Edit album', href: edit_album_path(@album.key))
     end
@@ -24,7 +24,6 @@ describe "albums/show" do
   context 'when logged in but not viewing own album' do
     it 'does not link to album edit' do
       view.stub(:current_user).and_return({ user_id: 2 })
-
       render
       expect(rendered).to_not have_link('Edit album', href: edit_album_path(@album.key))
     end
